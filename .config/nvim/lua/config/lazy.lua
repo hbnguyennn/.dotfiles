b03d49e6b2bb515bc6c484ec258bcfb5ed638c48
hbnguyennn <hoang.nguyen.rvc@gmail.com>
2026-02-23 13:58:50 +0000

    Update

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -51,3 +51,17 @@ require("lazy").setup({
     },
   },
 })
+
+require('min-theme').setup({
+  -- (note: if your configuration sets vim.o.background the following option will do nothing!)
+  theme = 'dark',      -- String: 'dark' or 'light', determines the colorscheme used
+  transparent = false, -- Boolean: Sets the background to transparent
+  italics = {
+    comments = false,  -- Boolean: Italicizes comments
+    keywords = false,  -- Boolean: Italicizes keywords
+    functions = false, -- Boolean: Italicizes functions
+    strings = false,   -- Boolean: Italicizes strings
+    variables = false, -- Boolean: Italicizes variables
+  },
+  overrides = {},      -- A dictionary of group names, can be a function returning a dictionary or a table.
+})
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,14 @@
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
+  { 'datsfilipe/min-theme.nvim' },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      -- colorscheme = "gruvbox",
+      colorscheme = "min-theme",
     },
   },
 }
